21ad249ee2bfb625ad64863194f7e817b1cc0160
Everardo Eugenio Ramirez <ever122511@gmail.com>
2016-12-06 11:17:03 -0600

    modificando

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@ if (@!$_SESSION['user']) {
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Proyecto Academias Ingenierias</title>
+    <title>Proyecto Academias Ingenierias en sistemas computacionales </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Joseph Godoy">
