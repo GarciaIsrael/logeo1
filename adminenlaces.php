8a9312840cc1b5f1cc079f3987b72ca0bd0a7a74
everardo <flavio>
2016-12-11 17:53:17 -0600

    actualizacionflavio

--- adminenlaces.php
+++ adminenlaces.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Proyecto Academias</title>
+    <title>GESTION DE PROYECTOS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Joseph Godoy">
