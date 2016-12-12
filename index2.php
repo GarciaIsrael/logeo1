1f60096db21d4321be488f3501ba524704fdd0bd
flavio50 <ga_nu@live.com.mx>
2016-12-11 18:19:02 -0600

    Update index2.php

--- index2.php
+++ index2.php
@@ -10,7 +10,7 @@ if (@!$_SESSION['user']) {
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>GESTION DE PROYECTOS</title>
+    <title>GESTION DE PROYECTOS 2</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Joseph Godoy">
